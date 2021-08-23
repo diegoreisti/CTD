9d8bb20862b90de672d6ca4c35691bff2a30f5e7
diegoreisti <disrplay@gmail.com>
2021-08-23 17:50:09 -0300

    Incluindo arquivos dentro da pasta CTD

--- 01 Bimestre/Front End/CTD-FrontEnd01/README.md
+++
@@ -1,2 +0,0 @@
-# CTD-FrontEnd01
-Matéria Front-End I no curso Certified Tech Dev 💻
--- 01 Bimestre/Programação Imperativa/Node/teste.js
+++
@@ -1,6 +0,0 @@
-// var a = ["a","b","c"]
-// a[100]="d"
-// console.log(a.length)
-const x = 6 % 2
-const y = x ? 'One' : 'Two'
-console.log(x)
