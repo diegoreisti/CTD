dbc7fb52f32da2126d075e5f49f3a9cbc6d61394
diegoreisti <disrplay@gmail.com>
2021-08-24 10:42:34 -0300

    Excluindo a pasta Node da matéria de prog imperat

---
+++ CTD/01 Bimestre/Programação Imperativa/teste.js
@@ -0,0 +1,13 @@
+/**
+ * PLANILHA DE DICAS - OPERADORES LÓGICOS
+ * 
+ * [ ! ] NEGADO
+ * [ tudo FALSO ] false, zero/0, vazios: '' ou "", null, undefined, NaN
+ * [ tudo VERDADEIRO ] true, '0', 'false', vazios: [], {}, function(){}
+ * [ == ] VALOR
+ * [ === ] OBJETO
+ */
+
+
+console.log("")
+
