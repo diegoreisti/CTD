fcfb7c6a3cc20e844fb62e66c918d5d4647fbf0c
diegoreisti <disrplay@gmail.com>
2021-08-25 20:42:19 -0300

    Apenas inserindo os enunciados de cada função

--- CTD/01 Bimestre/Programação Imperativa/aula09/praticaEstrutCondiconais.js
+++ CTD/01 Bimestre/Programação Imperativa/aula09/praticaEstrutCondiconais.js
@@ -1,4 +1,9 @@
-//1
+//Prática de Estruturas Condicionais
+
+/* 1 - Escreva uma função utilizando uma estrutura básica de IF/ELSE que verifique uma senha. A senha válida é o número 1234. Devem ser impressas as seguintes mensagens:
+	- ACESSO PERMITIDO caso a senha seja válida.
+	- ACESSO NEGADO caso a senha seja inválida. */
+
 function senha(psw){
     if (psw == 1234){
         return "ACESSO PERMITIDO"
@@ -7,7 +12,9 @@ function senha(psw){
 
 console.log(senha(1234))
 
-//2
+/* 2 - Escreva uma função utilizando a estrutura IF/ELSE-IF/ELSE que receba dois inteiros e diga qual deles é o maior, 
+qual deles é menor OU se são iguais. */
+
 function doisInteiros(a,b){
     if (a == b){
         return "NÚMEROS IGUAIS"
@@ -20,7 +27,9 @@ function doisInteiros(a,b){
 
 // console.log(doisInteiros(10,10))
 
-//3
+/* 3 - Escreva um programa utilizando a estrutura de switch que imprima o mês de acordo com o número fornecido.
+	- Exemplo: quando envio o número 9, o retorno será: 'SETEMBRO'*/
+
 function qualMes(m){
     let mes = ""
     switch (m){
@@ -55,7 +64,8 @@ function qualMes(m){
 
 // console.log(qualMes(9))
 
-//4
+//4 - Reescreva função do exercício 1, utilizando o operador ternário.
+
 function senhaOpTern(psw){
     return psw == 1234 ? "ACESSO PERMITIDO" : "ACESSO NEGADO"
 }
