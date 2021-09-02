9e2905f1825245318854578f4c1829e67399e65d
diegoreisti <disrplay@gmail.com>
2021-09-01 22:48:14 -0300

    Subindo aula 10

--- CTD/01 Bimestre/Programação Imperativa/teste.js
+++ CTD/01 Bimestre/Programação Imperativa/teste.js
@@ -9,5 +9,15 @@
  */
 
 
-console.log("Diego Reis".toLowerCase())
+// console.log("Diego Reis".toLowerCase())
+
+
+let nome = '4556364607935616'
+
+// return masked string
+function maskify(cc) {
+  return "#".repeat(cc.length-4) + cc.slice(-4)
+}
+
+console.log(maskify(nome))
 
