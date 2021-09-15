564541b950e8015917ab4e926e45c2ff21bd55b0
diegoreisti <disrplay@gmail.com>
2021-09-15 14:11:59 -0300

    Subindo  micro-desafio da Aula 13

--- CTD/01 Bimestre/Programação Imperativa/aula13/micro-desafio.js
+++ CTD/01 Bimestre/Programação Imperativa/aula13/micro-desafio.js
@@ -1,6 +1,6 @@
 function Conta(accountNumer, accountType, accountBalance, name) {
-    this.numero = accountNumer,
-    this.tipo = accountType,
+    this.numero = accountNumer
+    this.tipo = accountType
     this.saldo = accountBalance
     this.titular = name
     }
@@ -17,9 +17,47 @@ let ListaContas = [  new Conta(5486273622,	"Conta Corrente",	27771,	"Abigael Nat
                     ,new Conta(3151956165,	"Conta Corrente",	7601,	"Alonso Wannan")
                     ,new Conta(2138105881,	"Conta Poupança",	33196,	"Bendite Huggett") ]  
                   
-console.log(ListaContas)     
+// console.log(ListaContas)     
 
-let ListaContasJSON = JSON.stringify(ListaContas)
+// let ListaContasJSON = JSON.stringify(ListaContas)
 
-console.log(ListaContasJSON) 
+// console.log(ListaContasJSON) 
 
+let banco = {
+    clientes: ListaContas,
+    consultaCliente: function(titular) {
+        for (let i = 0; i < this.clientes.length; i++){
+            if (this.clientes[i].titular===titular){
+                console.log("Cliente "+this.clientes[i].titular+" encontrado");
+                return this.clientes[i]
+            }
+        }
+    },
+    depositar: function(titular,valor){
+       let conta = this.consultaCliente(titular) 
+        if (conta != undefined){
+           conta.saldo += valor;
+           console.log(`${conta.titular} Seu novo saldo é de ${conta.saldo}`);
+       }
+    },
+    saque: function(titular,valor){
+        let conta = this.consultaCliente(titular) 
+        if (conta != undefined){
+            if (conta.saldo >= valor){
+                conta.saldo -= valor
+                console.log(`Saque efetuado com sucesso. Seu saldo atual é de ${conta.saldo}`);
+            } else {
+                console.log("Saldo insuficiente para esta operação");
+            }
+
+        }
+    }
+}
+
+
+
+// banco.consultaCliente("Jacki Shurmer") 
+
+// banco.depositar("Jacki Shurmer",500) 
+
+banco.saque("Ramon Connell",8000)
