9efd4f69db12e4a37d5da9de82af0c0dd77853db
Diego <disrplay@gmail.com>
2021-09-22 19:06:23 -0300

    Mais métodos de arrays

--- CTD/01 Bimestre/Programação Imperativa/aula17/metodos-arrays-avancados.js
+++ CTD/01 Bimestre/Programação Imperativa/aula17/metodos-arrays-avancados.js
@@ -36,3 +36,55 @@ paises.forEach(function(pais){
     console.log(pais);
 })
 
+// slice
+
+let eu = 'Diego'
+
+let parteEu = eu.slice(0,3);
+console.log(parteEu);
+
+// splice(inicio, qtdItensRemover, itemAdicionar1, itemAdicionar2)
+let numerosSlc = [3,4,5,6,7]
+// @returns — An array containing the elements that were deleted.
+console.log('Tirando ' + numerosSlc.splice(0,2))
+
+console.log('Restou ' + numerosSlc);
+
+// sort
+// ordena
+console.log(paises.sort());
+
+// flat
+
+let numerosArray = [1,2,3,[4],[[5,6]],7] 
+
+let novoArray = numerosArray.flat()
+
+console.log(novoArray);
+
+novoArray = numerosArray.flat(2)
+
+console.log(novoArray);
+
+// includes
+
+let nomes = ['Diego','Maria','Carla','Fatima','Manu','Antonio','Maria','Roberto','Duda']
+
+console.log(nomes.includes('Duda'));
+console.log(nomes.includes('Celso'));
+
+// find
+
+let moedas = [
+    {nome: 'Bitcoin', simbolo: 'BTC'},
+    {nome: 'Bitcoin', simbolo: 'BTC'},
+    {nome: 'Ethereum', simbolo: 'ETH'},
+    {nome: 'Cardano', simbolo: 'ADA'},
+    {nome: 'Dega', simbolo: 'DGA'},
+]
+
+console.log(
+moedas.find(function(moeda){
+    return moeda.nome === 'Dega'
+})
+)
